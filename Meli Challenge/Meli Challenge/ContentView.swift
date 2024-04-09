e406c9e41d93999c2a69ec357fb4c8d419897873
Nahuel Jose Roldan <>
2024-04-09 17:42:50 -0300

    Create Xcode project

---
+++ Meli Challenge/Meli Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meli Challenge
+//
+//  Created by Nahuel Jose Roldan on 09/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meli Challenge/Meli Challenge/Meli_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meli_ChallengeApp.swift
+//  Meli Challenge
+//
+//  Created by Nahuel Jose Roldan on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Meli_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
